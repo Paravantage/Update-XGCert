ac502bf319e51179752fae1e31d53503fe6f1140
Gary Parr <gary.parr@paravantage.com>
2021-09-11 21:54:50 -0400

    Adding License

--- src/Update-XGCert.ps1
+++ src/Update-XGCert.ps1
@@ -1,4 +1,22 @@
 <#
+    Update-XGCert - for automating LE cert renewals in XG
+    Copyright (C) 2021  Paravantage, LLC
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#>
+
+<#
     .SYNOPSIS
     Performs Certificate Updates in Sophos XG Firewall
 
